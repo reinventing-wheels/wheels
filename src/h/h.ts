e036ebe07cc00136513110f4f80229c7b25aeafe
foo <5144598+bar@users.noreply.github.com>
2018-09-29 21:57:31 +0300

    feat: remove `h`

--- src/h/h.ts
+++
@@ -1,7 +0,0 @@
-import { Component, Props } from './types'
-import { isComponent } from './util'
-
-export const h = (tag: Component<Props> | string, props: any, ...children: any[]): JSX.Element =>
-  isComponent(tag)
-    ? tag({ children, ...props })
-    : { tag, children, props: props || {} }
