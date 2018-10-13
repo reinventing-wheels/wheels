691d8bc777e976629406729ebe3e925f29ab2149
foo <5144598+bar@users.noreply.github.com>
2018-10-13 11:34:59 +0300

    feat: `render` now also has a reference to passed object (`$` by default)

--- src/text/template.spec.ts
+++ src/text/template.spec.ts
@@ -5,14 +5,29 @@ describe('template', () => {
   const expected = 'Hello John Doe!'
 
   describe('renderer', () => {
-    it('should work', () => {
+    it('should have `$` by default', () => {
       const render = renderer('Hello ${$.firstName} ${$.lastName}!')
+      expect(render(user)).toBe(expected)
+    })
+
+    it('should be configurable', () => {
+      const render = renderer('Hello ${_.firstName} ${_.lastName}!', '_')
       expect(render(user)).toBe(expected)
     })
   })
 
   describe('render', () => {
-    it('should work', () => {
+    it('should have `$` by default', () => {
+      const rendered = render('Hello ${$.firstName} ${$.lastName}!', user)
+      expect(rendered).toBe(expected)
+    })
+
+    it('should be configurable', () => {
+      const rendered = render('Hello ${_.firstName} ${_.lastName}!', user, '_')
+      expect(rendered).toBe(expected)
+    })
+
+    it('should have destructured props', () => {
       const rendered = render('Hello ${firstName} ${lastName}!', user)
       expect(rendered).toBe(expected)
     })
--- src/text/template.ts
+++ src/text/template.ts
@@ -1,5 +1,5 @@
 export const renderer = (tmpl: string, arg = '$') =>
   new Function(arg, 'return `' + tmpl + '`')
 
-export const render = (tmpl: string, obj: object) =>
-  renderer(tmpl, '{' + Object.keys(obj) + '}')(obj)
+export const render = (tmpl: string, obj: object, arg = '$') =>
+  new Function(arg, '{' + Object.keys(obj) + '}', 'return `' + tmpl + '`')(obj, obj)
