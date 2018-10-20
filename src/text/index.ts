90b10bb08fc6407b8b4b2c34fcadc0a03cd2d4e5
foo <5144598+bar@users.noreply.github.com>
2018-10-20 11:16:02 +0300

    feat: add missing export

--- src/text/index.ts
+++ src/text/index.ts
@@ -1,8 +1,8 @@
 import * as styles from './styles'
 import * as format from './format'
+import * as template from './template'
 
-export { styles, format }
+export { styles, format, template }
 export * from './levenshtein'
 export * from './sanitize'
 export * from './split'
-
