3b03ef1cfb2d4cb05d757330f197102ca5bfaeb6
foo <5144598+bar@users.noreply.github.com>
2018-11-22 17:24:00 +0300

    feat: array shuffle

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,4 +1,13 @@
+import { Mutable } from '../types'
 import { random } from '../math'
 
 export const pick = <T>(items: ArrayLike<T>) =>
   items[items.length * random() | 0]
+
+export const shuffle = <T>(items: Mutable<ArrayLike<T>>): ArrayLike<T> => {
+  for (let i = items.length; i > 0;) {
+    const j = Math.floor(Math.random() * i--);
+    [items[i], items[j]] = [items[j], items[i]]
+  }
+  return items
+}
