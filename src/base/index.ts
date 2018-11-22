d12f7f719b29f42f59370396ed392138615b6102
foo <5144598+bar@users.noreply.github.com>
2018-11-22 19:22:38 +0300

    refactor

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,19 +1,19 @@
-export const encoder = (alphabet: string[]) => (bytes: Iterable<number>) =>
-  convert([...bytes], 0x100, alphabet.length).map(i => alphabet[i]).join('')
+export const encoder = (alphabet: string[]) => (input: Iterable<number>) =>
+  convert([...input], 0x100, alphabet.length).map(i => alphabet[i]).join('')
 
-export const decoder = (alphabet: string[]) => (chars: Iterable<string>) =>
-  convert([...chars].map(c => alphabet.indexOf(c)), alphabet.length, 0x100)
+export const decoder = (alphabet: string[]) => (input: Iterable<string>) =>
+  convert([...input].map(c => alphabet.indexOf(c)), alphabet.length, 0x100)
 
-export const convert = (bytes: number[], fromBase: number, toBase: number) => {
-  const acc = []
-  while (bytes.some(byte => byte > 0))
-    acc.push(divmod(bytes, fromBase, toBase))
-  return acc.reverse()
+export const convert = (input: number[], base: number, toBase: number) => {
+  const output = []
+  while (input.some(n => n > 0)) {
+    let remainder = 0
+    for (let i = 0; i < input.length; i++) {
+      const n = base * remainder + input[i]
+      input[i] = n / toBase >>> 0
+      remainder = n % toBase
+    }
+    output.push(remainder)
+  }
+  return output.reverse()
 }
-
-export const divmod = (bytes: number[], base: number, divisor: number) =>
-  bytes.reduce((remainder, byte, i, bytes) => {
-    const n = base*remainder + byte
-    bytes[i] = Math.floor(n/divisor)
-    return n%divisor
-  }, 0)
